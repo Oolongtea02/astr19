6aff2ee7156e6bef4c3a4d9da7ca750a2eedf890
Samuel Zhang <77314998+Oolongtea02@users.noreply.github.com>
2024-01-09 12:50:50 -0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#lecture1 Hello World
+#This is a comment
+#This program will write Hello World!
+
+#This function tells the computer to print hello world
+def PrintHelloWorld():
+    print("Hello World!")
+
+#This is the program that will be run by python
+def main():
+    PrintHelloWorld()
+
+if __name__=="__main__":
+    main()
