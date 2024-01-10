48cbac5ccf1fb6c2780014868a2365d748682ea8
Samuel Zhang <77314998+Oolongtea02@users.noreply.github.com>
2024-01-10 13:43:14 -0800

    Added Session 1 Prompt program

---
+++ Session1Prompt.py
@@ -0,0 +1,10 @@
+#session 1 prompt
+#This program will print out an introduction about me
+def AboutMe():
+    print("Name: Samuel Zhang \n Pronouns: He/Him \n My favorite movie is Train to Busan. I like horror movies that have character development. \n My favorite food is noodle soup. I like that I can add anything I want to it.")
+
+def main():
+    AboutMe()
+
+if __name__=="__main__":
+    main()
