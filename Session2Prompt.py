498ed3ec5841c8864ad719e451d47421e9625c61
Samuel Zhang <sashzhan@ucsc.edu>
2024-01-15 20:19:57 -0800

    Updated with fixed values

--- Session2Prompt.py
+++ Session2Prompt.py
@@ -2,22 +2,22 @@
 #This program will print the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer.
 
 def Sum_Numbers():
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = float(2)
+    num2 = float(1)
     sum = num1 + num2
     print("Sum of two numbers: ", sum)
     print(type(sum))
 
 def Difference_Numbers():
-    num1 = int(input("Enter an integer: "))
-    num2 = int(input("Enter another integer: "))
-    diff = abs(num1 - num2)
+    num1 = int(4)
+    num2 = int(6)
+    diff = num1 - num2
     print("Difference between two integers: ", diff)
     print(type(diff))
 
 def Product_Numbers():
-    num1 = float(input("Enter the first number: "))
-    num2 = int(input("Enter an integer: "))
+    num1 = float(6)
+    num2 = int(7)
     product = num1 * num2
     print("Product of a floating-point and an integer: ", product)
     print(type(product))
