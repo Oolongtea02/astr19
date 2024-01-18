1c72589c5c5ba1843cbb0682ee578792df3ae097
Samuel Zhang <sashzhan@ucsc.edu>
2024-01-17 19:53:30 -0800

    Added Session 4 Prompt for Code Journal 1

---
+++ Session4Prompt.py
@@ -0,0 +1,23 @@
+class fav_animal:
+    arms = float(5)
+    legs = float(7)
+    eyes = int(2)
+    tail = True
+    furry = False
+    print("Length of the arms is: ", arms)
+    print("Length of the legs is: ", legs)
+    print("Number of eyes there are: ", eyes)
+    print("Does it have a tail? ", tail)
+    print("Is it a furry: ", furry)
+
+
+
+def main():
+    fav_animal
+    
+    
+
+if __name__=="__main__":
+    main()
+
+    
